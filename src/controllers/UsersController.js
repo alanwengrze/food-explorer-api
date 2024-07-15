865cc01d9012723eb8bb9a4266b580534b1166c9
alan <alanwengrzee@gmail.com>
2024-07-15 06:49:45 -0300

    :sparkles: feat: create users route

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,13 +1,27 @@
 const AppError = require('../utils/AppError');
+const knex = require('../database/knex');
+const bcrypt = require('bcryptjs');
 class UsersController {
   async create(request, response) {
-    const { name, email } = request.body;
+    const { name, email, password } = request.body;
 
-    if(!name || !email) {
-      throw new AppError('Name and email are required', 400);
+    const emailExists = await knex("users").where({ email }).first();
+
+    if(emailExists) {
+      throw new AppError("Esse e-mail já está em uso.", 400);
     }
+
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+
+    const user = await knex("users").insert({
+      name,
+      email,
+      password: hashedPassword
+    }).returning("*");
     
-    response.status(201).json({name, email})
+    
+    response.status(201).json(user);
   }
 }
 
